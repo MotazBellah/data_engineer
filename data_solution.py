a4dfdfd86dd431afd5c370fa32d656e7c747d53a
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-12-14 23:16:59 +0200

    Use subqueries in order yo calculate the target currency to base currency in one query and convert the target value to related value in eur and usd

--- data_solution.py
+++ data_solution.py
@@ -1,11 +1,16 @@
 from data_setup import db
 
 def run():
-    data = db.execute('''select date_rate, target, source,(value*a_price) as output_value from rates, (SELECT CAST('2019-01-10' AS DATE) AS date_date, CAST('EUR' AS CHAR(3)) AS target_currency, CAST('11.11' AS NUMERIC(14,4)) AS a_price UNION
-                      SELECT CAST('2019-03-18' AS DATE) AS date_date, CAST('GBP' AS CHAR(3)) AS target_currency, CAST('12.12' AS NUMERIC(14,4)) AS a_price UNION
-                      SELECT CAST('2019-08-29' AS DATE) AS date_date, CAST('RUB' AS CHAR(3)) AS target_currency, CAST('333.33' AS NUMERIC(14,4)) AS a_price) as test
-                      where date_rate=date_date and target=target_currency and source='USD' ;''').fetchall()
-    return data
+    data = db.execute('''select date_rate, a_price, source,(value*a_price) as output_value, target from rates,
+                      (SELECT CAST('2019-01-10' AS DATE) AS date_date, CAST('EUR' AS CHAR(3)) AS target_currency,
+                      CAST('11.11' AS NUMERIC(14,4)) AS a_price UNION SELECT CAST('2019-03-18' AS DATE) AS date_date,
+                      CAST('GBP' AS CHAR(3)) AS target_currency, CAST('12.12' AS NUMERIC(14,4)) AS a_price UNION
+                      SELECT CAST('2019-08-29' AS DATE) AS date_date, CAST('RUB' AS CHAR(3)) AS target_currency,
+                      CAST('333.33' AS NUMERIC(14,4)) AS a_price) as target_data
+                      where date_rate=date_date and target=target_currency;''').fetchall()
+
+    for i in data:
+        print("At th date of {}, the {} {} equal to {} {}".format(*i))
 
 if __name__ == '__main__':
-    print(run())
+    run()
