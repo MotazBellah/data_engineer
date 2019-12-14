e8782f008846b3102972c95007202840bbf89d19
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-12-14 23:43:34 +0200

    return string output that contains the output of exection

--- data_solution.py
+++ data_solution.py
@@ -1,6 +1,7 @@
 from data_setup import db
 
 def run():
+    output = ''
     data = db.execute('''select date_rate, a_price, target,((1/value)*a_price) as output_value, source from rates,
                       (SELECT CAST('2019-01-10' AS DATE) AS date_date, CAST('EUR' AS CHAR(3)) AS target_currency,
                       CAST('11.11' AS NUMERIC(14,4)) AS a_price UNION SELECT CAST('2019-03-18' AS DATE) AS date_date,
@@ -10,7 +11,9 @@ def run():
                       where date_rate=date_date and target=target_currency;''').fetchall()
 
     for i in data:
-        print("At the date of {}, the {} {} equal to {} {}".format(*i))
+        output += "At the date of {}, the {} {} equal to {} {} \n".format(*i)
+
+    return output
 
 if __name__ == '__main__':
-    run()
+    print(run())
