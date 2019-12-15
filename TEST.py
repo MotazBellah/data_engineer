f2ef02da22b7e4786c8514721b0268d59b8a57f9
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-12-15 15:51:00 +0200

    add doc string for test functions

--- TEST.py
+++ TEST.py
@@ -5,6 +5,8 @@ from data_solution import run
 class TestRun(unittest.TestCase):
 
     def test_run_USD(self):
+        '''Convert all target data to USD value'''
+
         input = '''SELECT CAST('2019-08-09' AS DATE) AS date_date, CAST('CAD' AS CHAR(3)) AS target_currency,
                 CAST('675.34' AS NUMERIC(14,4)) AS a_price UNION SELECT CAST('2019-03-13' AS DATE) AS date_date,
                 CAST('GBP' AS CHAR(3)) AS target_currency, CAST('52' AS NUMERIC(14,4)) AS a_price UNION
@@ -16,24 +18,28 @@ class TestRun(unittest.TestCase):
         self.assertEqual(response, excepected_output)
 
     def test_run_EUR(self):
+        '''Convert all target data to EUR value'''
+
         input = '''SELECT CAST('2019-08-09' AS DATE) AS date_date, CAST('CAD' AS CHAR(3)) AS target_currency,
                 CAST('675.34' AS NUMERIC(14,4)) AS a_price UNION SELECT CAST('2019-03-13' AS DATE) AS date_date,
                 CAST('GBP' AS CHAR(3)) AS target_currency, CAST('52' AS NUMERIC(14,4)) AS a_price UNION
                 SELECT CAST('2019-06-11' AS DATE) AS date_date, CAST('RUB' AS CHAR(3)) AS target_currency,
                 CAST('3.5' AS NUMERIC(14,4)) AS a_price'''
         response = run(input, 'EUR')
-        # print(response)
+
         excepected_output = '''At the date of 2019-06-11, the 3.5000 RUB equal to 0.04793 EUR \nAt the date of 2019-03-13, the 52.0000 GBP equal to 60.54960 EUR \nAt the date of 2019-08-09, the 675.3400 CAD equal to 456.43417 EUR'''
         self.assertEqual(response, excepected_output)
 
     def test_run_DATE(self):
+        '''Test the response a gainst incorrect date (future or past)'''
+        
         input = '''SELECT CAST('2022-08-09' AS DATE) AS date_date, CAST('CAD' AS CHAR(3)) AS target_currency,
                 CAST('675.34' AS NUMERIC(14,4)) AS a_price UNION SELECT CAST('2000-03-13' AS DATE) AS date_date,
                 CAST('GBP' AS CHAR(3)) AS target_currency, CAST('52' AS NUMERIC(14,4)) AS a_price UNION
                 SELECT CAST('1800-06-11' AS DATE) AS date_date, CAST('RUB' AS CHAR(3)) AS target_currency,
                 CAST('3.5' AS NUMERIC(14,4)) AS a_price'''
         response = run(input)
-        # print(response)
+
         excepected_output = '''Please check the date or the target currency!'''
         self.assertEqual(response, excepected_output)
 
