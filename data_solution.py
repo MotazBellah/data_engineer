8c7cee323dcfc18f5d065939153dcddda2bd5c5f
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-12-15 14:28:44 +0200

    add argument to run function

--- data_solution.py
+++ data_solution.py
@@ -1,14 +1,14 @@
 from data_setup import db
 
-def run():
+def run(target_data):
     output = ''
-    data = db.execute('''select date_rate, a_price, target,((1/value)*a_price) as output_value, source from rates,
-                      (SELECT CAST('2019-01-10' AS DATE) AS date_date, CAST('EUR' AS CHAR(3)) AS target_currency,
-                      CAST('11.11' AS NUMERIC(14,4)) AS a_price UNION SELECT CAST('2019-03-18' AS DATE) AS date_date,
-                      CAST('GBP' AS CHAR(3)) AS target_currency, CAST('12.12' AS NUMERIC(14,4)) AS a_price UNION
-                      SELECT CAST('2019-08-29' AS DATE) AS date_date, CAST('RUB' AS CHAR(3)) AS target_currency,
-                      CAST('333.33' AS NUMERIC(14,4)) AS a_price) as target_data
-                      where date_rate=date_date and target=target_currency;''').fetchall()
+    if target_data:
+        data = db.execute('''select date_rate, a_price, target,((1/value)*a_price) as output_value, source from rates,
+                          ({}) as target_data
+                          where date_rate=date_date and target=target_currency;'''.format(target_data)).fetchall()
+    else:
+        return output
+        
     if data:
         for i in data:
             output += "At the date of {}, the {} {} equal to {} {} \n".format(*i)
@@ -16,4 +16,8 @@ def run():
     return output
 
 if __name__ == '__main__':
-    print(run())
+    print(run('''SELECT CAST('2019-01-10' AS DATE) AS date_date, CAST('EUR' AS CHAR(3)) AS target_currency,
+              CAST('11.11' AS NUMERIC(14,4)) AS a_price UNION SELECT CAST('2019-03-18' AS DATE) AS date_date,
+              CAST('GBP' AS CHAR(3)) AS target_currency, CAST('12.12' AS NUMERIC(14,4)) AS a_price UNION
+              SELECT CAST('2019-08-29' AS DATE) AS date_date, CAST('RUB' AS CHAR(3)) AS target_currency,
+              CAST('333.33' AS NUMERIC(14,4)) AS a_price'''))
